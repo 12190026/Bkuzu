51862dc98b486463f815d2cbc04fa60e8dd5c81f
12190026 <12190026.gcit@rub.edu.bt>
2022-11-28 17:37:10 +0600

    Works
    Res

--- server/index.js
+++ server/index.js
@@ -16,6 +16,12 @@ const {MONGODB_URI} = require("./config")
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static("public"));
+// app.use(express.static(path.join(__dirname, "/client/build")));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+// });
 
 app.use((req, res, next) => {
   io.req = req
